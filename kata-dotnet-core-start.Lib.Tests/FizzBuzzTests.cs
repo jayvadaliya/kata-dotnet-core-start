5a123f9202d0629370928adb2396978711066c19
tsukhwani <tsukhwani@gantner.com>
2022-12-22 17:07:38 +0530

    Test case for generate

--- kata-dotnet-core-start.Lib.Tests/FizzBuzzTests.cs
+++ kata-dotnet-core-start.Lib.Tests/FizzBuzzTests.cs
@@ -51,9 +51,10 @@ public class FizzBuzzTests
     public void WhenGenerateFizzBuzzShoulReturnExpectedOutput()
     {
         //Arrange
-        var expectedOutput = "12Fizz";
+        var expectedOutput = "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829FizzBuzz3132Fizz34BuzzFizz3738FizzBuzz41Fizz4344FizzBuzz4647Fizz49BuzzFizz5253FizzBuzz56Fizz5859FizzBuzz6162Fizz64BuzzFizz6768FizzBuzz71Fizz7374FizzBuzz7677Fizz79BuzzFizz8283FizzBuzz86Fizz8889FizzBuzz9192Fizz94BuzzFizz9798FizzBuzz";
         //Act
         var result = FizzBuzz.Generate();
+        result = result.Replace("\n", "");
         //Assert
         Assert.Equal(expectedOutput, result);
     }
