fd875e8b3c92f69faa5d82e5065c381d3dc34bff
tsukhwani <tsukhwani@gantner.com>
2022-12-22 15:28:50 +0530

    initial commit

--- kata-dotnet-core-start.Lib.Tests/UnitTest1.cs
+++ kata-dotnet-core-start.Lib.Tests/UnitTest1.cs
@@ -2,9 +2,18 @@ namespace kata_dotnet_core_start.Lib.Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Theory] 
+    [InlineData(1,"1")]
+    [InlineData(3,"Fizz")]
+    [InlineData(5,"Buzz")]
+    [InlineData(15,"FizzBuzz")]
+    public void CheckFizzBuzz(int input, string output)
     {
-        Assert.Equal(true, false);
+        //Arrange
+        
+        //Act
+        var result = FizzBuzz.GetFizzBuzz(input);
+        //Assert
+        Assert.Equal(output, result);
     }
 }
--- kata-dotnet-core-start.Lib/Class1.cs
+++ kata-dotnet-core-start.Lib/Class1.cs
@@ -1,5 +1,25 @@
 ﻿namespace kata_dotnet_core_start.Lib;
-public class Class1
+public static class FizzBuzz
 {
+    public static string GetFizzBuzz(int input)
+    {
+        //TODO : refactor
+        if (input % 3 == 0 && input % 5 == 0)
+        {
+            return "FizzBuzz";
+        }
+        else if (input % 3 == 0)
+        {
+            return "Fizz";
+        }
+        else if (input % 5 == 0)
+        {
+            return "Buzz";
+        }
+        else
+        {
+            return input.ToString();
+        }
+    }
 
 }
