c52ece4b3473eb5c3ecdc314851a02e3be26a12d
JVadaliya <JVadaliya@gantner.com>
2024-09-06 14:53:59 +0530

    Initialize mars map

---
+++ kata-dotnet-core-start.Lib.Tests/MarsMapTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace kata_dotnet_core_start.Lib.Tests;
+
+public class MarsMapTests
+{
+    [Fact]
+    public void Initialize_MarsMap_Then_DefaultRowAndColumnInitialize()
+    {
+        // Act
+        var result = new MarsMap();
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(5, result.Row);
+        Assert.Equal(5, result.Column);
+    }
+}
---
+++ kata-dotnet-core-start.Lib/MarsMap.cs
@@ -0,0 +1,14 @@
+﻿namespace kata_dotnet_core_start.Lib;
+
+public class MarsMap
+{
+    public MarsMap()
+    {
+        Row = 5;
+        Column = 5;
+    }
+
+    public int Row { get; set; }
+    
+    public int Column { get; set; }
+}
